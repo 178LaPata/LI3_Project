a84eed5880125ee43d52f90357bf04637d60047e
178LaPata <larapinto697@gmail.com>
2022-11-19 16:42:43 +0000

    oi

--- src/users.c
+++ src/users.c
@@ -62,24 +62,14 @@ int set_users_birth_date (XD_USERS user, char* line){
 }
 
 int set_users_account_creation (XD_USERS user, char* line){
-    if (strlen(line) ) return 0;
+    if (strlen(line)<19) return 0;    
 
-}
+    sscanf(line, "%d-%d-%d %d:%d:%d", &user->account_creation.tm_year, &user->account_creation.tm_mon, &user->account_creation.tm_mday, &user->account_creation.tm_hour, &user->account_creation.tm_min, &user->account_creation.tm_sec);
+    user->account_creation.tm_year -= 1900;
+    user->account_creation.tm_mon -= 1;
+    user->account_creation.tm_isdst = 1;
+    user->account_creation.tm_wday = 3;
+    user->account_creation.tm_yday = (user->account_creation.tm_mon * 30) + user->account_creation.tm_mday;
 
-
-
-int set_created_at (char* line, GH_USER usr)  // Funçao utilizada para preencher o campo da data de criaçao
-{
-    if (strlen(line)<19) return 0;   // Se a linha tiver menos de 19 caracteres não tem informação suficente para fazer o parse
-
-
-    //Coloca na data os valores segundo o formato especificado
-    sscanf(line, "%d-%d-%d %d:%d:%d", &usr->created_at.tm_year, &usr->created_at.tm_mon, &usr->created_at.tm_mday, &usr->created_at.tm_hour, &usr->created_at.tm_min, &usr->created_at.tm_sec);
-    usr->created_at.tm_year -= 1900; // Subtrai 1900 ao ano (porque na estrutura struct tm, o campo tm_year conta o numero de anos que passou desde 1900)
-    usr->created_at.tm_mon -= 1;     // Subtrai 1 ao mes (porque na estrutura struct tm, o campo tm_mon conta o numero de meses que passou desde janeiro)
-    usr->created_at.tm_isdst = 1;
-    usr->created_at.tm_wday = 3;
-    usr->created_at.tm_yday = (usr->created_at.tm_mon * 30) + usr->created_at.tm_mday;
-
-    return (line[4]!='/') && (is_valid_date(usr->created_at));   // Retorna o valor que a função retorna, que é verdadeiro se estiver entre a data atual e 2005-07-04 00:00:00
+    return (line[4]!='/') && (is_valid_date(user->account_creation));
 }
